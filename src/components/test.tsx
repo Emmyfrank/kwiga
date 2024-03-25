6766a1d6efd65955a6685455f6009e2f5c0ac73f
Ngendahimana Emmanuel <86968559+Emmyfrank@users.noreply.github.com>
2024-03-25 12:31:39 +0200

    Ft test3 (#5)
    * here we go
    
    * added css file
    
    * second commit
    
    * testing2css
    
    * second commit
    
    * comflict2

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -6,6 +6,7 @@ export default function test() {
       <h1>i would like to test if this can work</h1>
       <p>i can see this are going to work</p>
       <p>test 3</p>
+      <h3>comflict2</h3>
     </div>
   )
 }
